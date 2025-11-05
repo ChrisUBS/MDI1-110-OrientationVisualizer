9340a8e859b7c99d7daea33efe7f64fb6331055e
Christian Bonilla <christianurielbonillasuarez@gmail.com>
2025-11-04 22:27:34 -0800

    Class 3

---
+++ MDI1-110-OrientationVisualizer/MDI1_110_OrientationVisualizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MDI1_110_OrientationVisualizerApp.swift
+//  MDI1-110-OrientationVisualizer
+//
+//  Created by Christian Bonilla on 03/11/25.
+//
+
+import SwiftUI
+
+@main
+struct MDI1_110_OrientationVisualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            OrientationRootView()
+        }
+    }
+}
